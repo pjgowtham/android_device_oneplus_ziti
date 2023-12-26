d3210bd561e9f53af3d9ed0be822520c213e758a
pjgowtham <pjgowtham@gmail.com>
2023-12-26 19:02:51 +0530

    ziti: Initial rebrand for OnePlus Nord CE3 5G
    Change-Id: I1d7decc0d1085ede4e1625f0b293a1cb43a916ee

--- lineage_ziti.mk
+++ lineage_ziti.mk
@@ -9,24 +9,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from lemonade device
-$(call inherit-product, device/oneplus/lemonade/device.mk)
+# Inherit from ziti device
+$(call inherit-product, device/oneplus/ziti/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lemonade
-PRODUCT_DEVICE := lemonade
+PRODUCT_NAME := lineage_ziti
+PRODUCT_DEVICE := ziti
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := LE2115
+PRODUCT_MODEL := CPH2569
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="OnePlus9-user 14 UKQ1.230924.001 R.1b54031-8e6e-42a35 release-keys" \
-    BuildFingerprint=OnePlus/OnePlus9/OnePlus9:14/UKQ1.230924.001/R.1b54031-8e6e-42a35:user/release-keys \
-    DeviceName=OnePlus9 \
-    DeviceProduct=OnePlus9 \
-    SystemDevice=OnePlus9 \
-    SystemName=OnePlus9
+    BuildDesc="CPH2569-user 14 UKQ1.230924.001 T.R4T2.194400b_2_1 release-keys" \
+    BuildFingerprint=OnePlus/CPH2569/OP5953L1:14/UKQ1.230924.001/T.R4T2.194400b_2_1:user/release-keys \
+    DeviceName=OP5953L1 \
+    DeviceProduct=CPH2569 \
+    SystemDevice=OP5953L1 \
+    SystemName=CPH2569
