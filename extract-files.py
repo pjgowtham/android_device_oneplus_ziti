2d41e3ed72096abfd296b3598ef9f5791e93067f
pjgowtham <pjgowtham@gmail.com>
2024-11-12 18:26:24 +0530

    ziti: Avoid patching QTI Camera postproc

--- extract-files.py
+++ extract-files.py
@@ -100,8 +100,6 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/etc/libnfc-nxp.conf': blob_fixup()
         .regex_replace('(NXPLOG_.*_LOGLEVEL)=0x03', '\\1=0x02')
         .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
-    'vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so': blob_fixup()
-        .call(blob_fixup_nop_call, 'bl', '__cfi_check', '_ZN7android8hardware22configureRpcThreadpoolEmb@plt'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
