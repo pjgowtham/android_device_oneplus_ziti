c596aa621b1411d6c22af532dca8eac95312f331
Bruno Martins <bgcngm@gmail.com>
2025-04-30 00:28:16 +0100

    ziti: Set Lineage Health control charging path
    This must now be set per device after migrating to soong_config_set.
    
    Change-Id: I5dd2801391b0e790bafa81bde81fe2d7b737fb30

--- device.mk
+++ device.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2024 The LineageOS Project
+# Copyright (C) 2021-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -33,6 +33,9 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.ir-service.oplus \
+
+# Lineage Health
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/oplus_chg/battery/mmi_charging_enable)
 
 # NFC
 PRODUCT_PACKAGES += \
